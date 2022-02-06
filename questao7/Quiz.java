c7da9909908a74e3d2406a7cc77e9b8f55f5ce17
fsclaudio <fsclaudio@gmail.com>
2022-02-05 23:18:42 -0300

    Add Class Quiz

---
+++ questao7/Quiz.java
@@ -0,0 +1,24 @@
+package questao7;
+public class Quiz {
+
+	private String pergunta;
+	private String resposta;
+	public Quiz(String pergunta, String resposta) {
+	
+		this.pergunta = pergunta;
+		this.resposta = resposta;
+	}
+	public String getPergunta() {
+		return pergunta;
+	}
+	public void setPergunta(String pergunta) {
+		this.pergunta = pergunta;
+	}
+	public String getResposta() {
+		return resposta;
+	}
+	public void setResposta(String resposta) {
+		this.resposta = resposta;
+	}
+
+}
