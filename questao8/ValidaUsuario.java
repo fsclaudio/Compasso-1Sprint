ff99e9d620767abdb4ee8d1eda7bb422c7c988a4
fsclaudio <fsclaudio@gmail.com>
2022-02-06 16:33:32 -0300

    Add functionality cleanScreen

--- questao8/ValidaUsuario.java
+++ questao8/ValidaUsuario.java
@@ -16,30 +16,36 @@ public class ValidaUsuario {
 		System.out.println("Entre com a Senha");
 
 		String userPassword = sc.nextLine();
+		
+		clearScreen();
 
 		for (int i = 0; i < usuarios.size(); i++) {
 			if ((usuarios.get(i).getUsario().equals(userName)) && (usuarios.get(i).getSenha().equals(userPassword))) {
 				exibeMesangem();
-				System.out.print(" Voce logo ao nosso sistema");
 				break;
 			} else {
-				System.out.println("Usuario e/ou Senha incorretos!");
+				System.out.println("Usuario ou Senha Incorretos!");
 				break;
 			}
 		}
+		sc.close();
 	}
 
 	public static void exibeMesangem() {
 		LocalTime presentTime = LocalTime.now();
 		if (presentTime.getHour() <= 5) {
-			System.out.println("Boa Madrugada!");
+			System.out.println("Boa Madrugada Voce logou ao nosso sistema!");
 		} else if (presentTime.getHour() < 12) {
-			System.out.println("Bom dia!");
+			System.out.println("Bom dia Voce logou ao nosso sistema!");
 		} else if (presentTime.getHour() < 18) {
-			System.out.println("Boa Tarde!");
+			System.out.println("Boa Tarde Voce logou ao nosso sistema!");
 		} else {
-			System.out.println("Boa Noite!");
+			System.out.println("Boa Noite Voce logo ao nosso sistema!");
 		}
 	}
+	public static void clearScreen() {  
+		 for (int i = 0; i < 100; ++i)  
+		       System.out.println();  
+	} 
 
 }
